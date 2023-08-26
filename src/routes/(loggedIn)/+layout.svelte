b4ca547d66d302da7450e493a3f4479151818c92
qwacko <james@jmcdowall.com>
2023-08-26 20:37:30 +0000

    Added Signout Page

--- src/routes/(loggedIn)/+layout.svelte
+++ src/routes/(loggedIn)/+layout.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-
-	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+	import { Dropdown, DropdownItem, Heading } from 'flowbite-svelte';
+	import MenuIcon from '~icons/mdi/menu';
 
 	$: activeUrl = $page.url.pathname;
 </script>
 
-<div class="p-4">
-	<Sidebar {activeUrl}>
-		<SidebarWrapper>
-			<SidebarGroup>
-				<SidebarItem label="Users" href="/users" />
-				<SidebarItem label="User" href="/user" />
-			</SidebarGroup></SidebarWrapper
-		>
-	</Sidebar>
-	<slot />
+<div class="flex flex-col gap-2">
+	<div class="m-x-10 m-y-4 flex w-full flex-row items-center justify-center gap-10 p-4">
+		<Heading level="h1" class="text-2xl font-bold">Snack Shack</Heading>
+		<div class="flex flex-grow" />
+		<MenuIcon class="dots-menu" />
+		<Dropdown triggeredBy=".dots-menu" {activeUrl}>
+			<DropdownItem href="/users" activeClass="font-bold">Users</DropdownItem>
+			<DropdownItem slot="footer" href="/signout">Sign out</DropdownItem>
+		</Dropdown>
+	</div>
+
+	<div class="flex">
+		<slot />
+	</div>
 </div>
---
+++ src/routes/(loggedIn)/signout/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import Center from '$lib/components/Center.svelte';
+	import { Heading, Button } from 'flowbite-svelte';
+</script>
+
+<form method="POST" use:enhance class="mt-4 flex w-full items-center justify-center">
+	<Button type="submit">Sign Out</Button>
+</form>
