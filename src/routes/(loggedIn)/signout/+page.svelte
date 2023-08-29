05fe33285a727a8f1b56fe4faa0088e5de9cbfd8
qwacko <james@jmcdowall.com>
2023-08-29 00:04:20 +0000

    Signout improvement

--- src/routes/(loggedIn)/signout/+page.svelte
+++ src/routes/(loggedIn)/signout/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import Center from '$lib/components/Center.svelte';
+	import PageLayout from '$lib/components/PageLayout.svelte';
 	import { Heading, Button } from 'flowbite-svelte';
 </script>
 
-<form method="POST" use:enhance class="mt-4 flex w-full items-center justify-center">
-	<Button type="submit">Sign Out</Button>
-</form>
+<PageLayout title="Sign Out">
+	<form method="POST" use:enhance class="mt-4 flex w-full items-center justify-center">
+		<Button type="submit">Sign Out</Button>
+	</form>
+</PageLayout>
