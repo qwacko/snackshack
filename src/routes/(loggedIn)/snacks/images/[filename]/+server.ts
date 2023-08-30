0374a488e3529a99bbd1bf9eafade7df8cabab0d
qwacko <james@jmcdowall.com>
2023-08-30 09:29:23 +0000

    Fixed Image Error

--- src/routes/(loggedIn)/snacks/images/[filename]/+server.ts
+++ src/routes/(loggedIn)/snacks/images/[filename]/+server.ts
@@ -4,14 +4,12 @@ import { readFileSync } from 'fs';
 import { logging } from '$lib/server/logging';
 
 export const GET = (async ({ params }) => {
-	logging.info('Getting image', params.filename);
-
 	let img: Buffer;
 
 	try {
 		img = readFileSync(`uploads/${params.filename}`);
 	} catch (e) {
-		logging.error('Error reading file', e);
+		logging.error('Getting Image : Error reading file', e);
 		throw error(500, 'Image not found');
 	}
 
