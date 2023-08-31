70a9ccf8926f62dc69d6710f637558ecea6a05cd
qwacko <james@jmcdowall.com>
2023-08-31 10:00:39 +0000

    Tidied up a coueple of docker items.

--- dockerEntrypoint.sh
+++ dockerEntrypoint.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 echo '---- Starting Server ----'
-
-echo '---- ls /app ----'
-ls -ln /app
-
-echo '---- ls /app/biuild ----'
-ls -ln /app/build
-
-echo '---- ls /schema ----'
-ls -ln /app/src/lib/server/db/migrations
-
-echo '---- Run ----'
 node -r dotenv/config build
